2f7ff5c3e01c7ae55d60ade3f32f6cfac484e558
Ashleigh Adams <ashleighadams318@hotmail.co.uk>
2016-08-16 08:48:29 +0100

    homework submission

---
+++ app/src/main/java/com/example/user/homework_app/HelloHomework.java
@@ -0,0 +1,15 @@
+package com.example.user.homework_app;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by user on 15/08/2016.
+ */
+public class HelloHomework extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
